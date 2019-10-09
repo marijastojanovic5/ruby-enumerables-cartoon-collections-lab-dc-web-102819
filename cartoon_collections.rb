23946868d735a7dcd9e644c0d8df6ff1821b63e9
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-09 18:01:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (array )
+  array.each_with_index do |dwarve,current_index|
+  puts (current_index+1).to_s + dwarve  
+  end
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|planet| planet.capitalize+"!" }
+  #planeteer_calls.collect! { |planet| puts planet + "!" }
 end
 
 def long_planeteer_calls# code an argument here
