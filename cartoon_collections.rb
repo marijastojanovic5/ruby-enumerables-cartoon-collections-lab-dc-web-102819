f5a9a6841ca2c1a10fb4e2d30788ed67f58088f9
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-09 18:09:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(planeteer_calls)
   #planeteer_calls.collect! { |planet| puts planet + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.select{|call|call>4}
 end
 
 def find_the_cheese# code an argument here
