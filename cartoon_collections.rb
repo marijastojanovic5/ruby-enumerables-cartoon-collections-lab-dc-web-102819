d53070a3cf27bcf724ba5350231f80a67fc59388
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-10-09 18:12:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
- 
-  cheese_types = ["cheddar", "gouda", "camembert"]
+ cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find { |cheese| cheese_types.include?(cheese) }
 end
